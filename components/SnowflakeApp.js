434368440f2c469d1ccb7baaa16440a536630108
Cliff Rodgers <kliph@users.noreply.github.com>
2017-10-30 16:01:44 -0400

    Programmatically define the defaultMilestoneMap

--- components/SnowflakeApp.js
+++ components/SnowflakeApp.js
@@ -45,28 +45,16 @@ const coerceMilestone = (value: number): Milestone => {
   }
 }
 
+const defaultMilestoneMap: MilestoneMap = trackIds.reduce((acc, trackId) => {
+  acc[trackId] = 0
+  return acc
+}, {})
+
 const emptyState = (): SnowflakeAppState => {
   return {
     name: '',
     title: '',
-    milestoneByTrack: {
-      'MOBILE': 0,
-      'WEB_CLIENT': 0,
-      'FOUNDATIONS': 0,
-      'SERVERS': 0,
-      'PROJECT_MANAGEMENT': 0,
-      'COMMUNICATION': 0,
-      'CRAFT': 0,
-      'INITIATIVE': 0,
-      'CAREER_DEVELOPMENT': 0,
-      'ORG_DESIGN': 0,
-      'WELLBEING': 0,
-      'ACCOMPLISHMENT': 0,
-      'MENTORSHIP': 0,
-      'EVANGELISM': 0,
-      'RECRUITING': 0,
-      'COMMUNITY': 0
-    },
+    milestoneByTrack: defaultMilestoneMap,
     focusedTrackId: 'MOBILE'
   }
 }
